c2d884aaac86cbc6e7c76031f138d2a87818f7c7
Musa Jubril <musa4jubril@gmail.com>
2025-03-03 12:02:17 +0100

    chnage typecheck strictness to false

--- src/app/what-we-do/[slug]/layout.tsx
+++ src/app/what-we-do/[slug]/layout.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 
 interface paramsProps { slug: string };
-export default function layout({ children, params }: { children: React.ReactNode, params: paramsProps}) {
+export default function layout({ children, params }: { children: React.ReactNode, params: { slug: string }}) {
     const pathname = usePathname();
     const slugData = WhatWeDo.find(_=> _.slug===params.slug)
         console.log(slugData)
